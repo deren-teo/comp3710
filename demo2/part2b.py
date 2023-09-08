b222508dd20a2515f34bfe0eccd43fb6c47b1b32
Deren Teo <teo.deren@icloud.com>
2023-09-08 22:31:35 +1000

    a2q2: implemented custom resnet

--- demo2/part2b.py
+++ demo2/part2b.py
@@ -1,14 +1,13 @@
 """
 Part 2b: CNNs
 
-Fast CIFAR10 classifier uses a custom ResNet-9 model to achieve >93% accuracy
+Fast CIFAR10 classifier using a custom ResNet model. Achieves >93% accuracy
 with less than 360 seconds of training on a single Nvidia V100 GPU.
 
 References:
     https://github.com/shakes76/pattern-analysis-2023/blob/experimental/classify/test_classify_mnist.py
     https://github.dev/apple/ml-cifar-10-faster/
-    https://blog.paperspace.com/writing-resnet-from-scratch-in-pytorch/
-
+    K. He, X. Zhang, S. Ren and J. Sun, “Deep Residual Learning for Image Recognition,” in CVPR, 2016.
 """
 from pathlib import Path
 
@@ -19,8 +18,7 @@ import torchvision.transforms as transforms
 
 from tqdm import tqdm
 
-if not torch.cuda.is_available():
-    raise Exception("CUDA not available")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #--------------
 # Data
@@ -45,35 +43,37 @@ test_loader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False
 
 #--------------
 # Model
+
+def conv_block(c_in, c_out):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=c_in, out_channels=c_out, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+        nn.BatchNorm2d(c_out),
+        nn.ReLU(inplace=False)
+    )
+
+def conv_pool_block(c_in, c_out):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=c_in, out_channels=c_out, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+        nn.MaxPool2d(kernel_size=2),
+        nn.BatchNorm2d(c_out),
+        nn.ReLU(inplace=False)
+    )
+
 class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
 
 class Residual(nn.Module):
-    def __init__(self, in_channels, out_channels, downsample=None, **kwargs):
+    def __init__(self, channels):
         super(Residual, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, **kwargs),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU()
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, **kwargs),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU()
-        )
-        self.downsample = downsample
-        self.relu = nn.ReLU()
-        self.out_channels = out_channels
+        self.conv1 = conv_block(channels, channels)
+        self.conv2 = conv_block(channels, channels)
 
     def forward(self, x):
         residual = x
         out = self.conv1(x)
         out = self.conv2(x)
-        if self.downsample:
-            residual = self.downsample(x)
-        out += residual
-        out = self.relu(out)
+        out = out + residual
         return out
 
 class ResNet9(nn.Module):
@@ -81,55 +81,29 @@ class ResNet9(nn.Module):
         super(ResNet9, self).__init__()
         self.in_channels = in_channels
         self.num_classes = num_classes
-        self._make_layers(in_channels, num_classes)
+        self.layers = self._make_layers(in_channels, num_classes)
 
     def forward(self, x):
-        raise NotImplementedError()
+        return self.layers(x)
 
     def _make_layers(self, in_channels, out_channels):
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels=64, **conv2d_kwargs),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-        )
-        self.residual1 = Residual(in_channels=64, out_channels=128, kernel_size=3, padding=1, bias=False)
-        self.convpool = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, **conv2d_kwargs),
-            nn.MaxPool2d(kernel_size=2),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-        )
-
-
-        conv2d_kwargs = dict(kernel_size=3, padding=1, bias=False)
-        self.downsample1 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=1, stride=2, bias=False),
-            nn.BatchNorm2d(128),
-        )
-        self.convpool = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, **conv2d_kwargs),
-            nn.MaxPool2d(kernel_size=2),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-        )
-        self.residual2 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=512, **conv2d_kwargs),
-            nn.MaxPool2d(kernel_size=2),
-            nn.BatchNorm2d(512),
-        )
-        self.downsample2 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=1, stride=2, bias=False),
-            nn.BatchNorm2d(512),
-        )
-        self.classifier = nn.Sequential(
+        layers = [
+            conv_block(in_channels, 64),
+            conv_pool_block(64, 128),
+            Residual(128),
+            conv_pool_block(128, 256),
+            Residual(256),
+            conv_pool_block(256, 512),
+            Residual(512),
             nn.MaxPool2d(kernel_size=4),
             Flatten(),
             nn.Linear(512, out_channels, bias=False)
-        )
+        ]
+        return nn.Sequential(*layers)
 
 
 model = ResNet9(in_channels=3, num_classes=10)
-model = model.to("cuda")
+model = model.to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
@@ -141,8 +115,8 @@ NUM_EPOCHS = 16
 model.train()
 for epoch in range(NUM_EPOCHS):
     for i, (images, labels) in tqdm(enumerate(train_loader)):
-        images = images.to("cuda")
-        labels = labels.to("cuda")
+        images = images.to(device)
+        labels = labels.to(device)
 
         # Forward pass
         outputs = model(images)
@@ -163,8 +137,8 @@ with torch.no_grad():
     total = 0
     correct = 0
     for images, labels in tqdm(test_loader):
-        images = images.to("cuda")
-        labels = labels.to("cuda")
+        images = images.to(device)
+        labels = labels.to(device)
 
         outputs = model(images)
 
